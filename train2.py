767a23a206e3facebbe9bf591be5a99f660a92aa
Arpit <dropmailtoarpit@gmail.com>
2026-05-23 09:52:57 +0530

    Added Kernel Ridge model and GitHub Actions pipeline

---
+++ train2.py
@@ -0,0 +1,29 @@
+from sklearn.kernel_ridge import KernelRidge
+
+from misc import (
+    load_data,
+    preprocess_data,
+    train_model,
+    evaluate_model
+)
+
+
+df = load_data()
+
+X_train, X_test, y_train, y_test = preprocess_data(df)
+
+model = KernelRidge()
+
+trained_model = train_model(
+    model,
+    X_train,
+    y_train
+)
+
+mse = evaluate_model(
+    trained_model,
+    X_test,
+    y_test
+)
+
+print("Kernel Ridge MSE:", mse) 
